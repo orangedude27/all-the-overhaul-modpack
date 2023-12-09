8b0e4b24b3950de455fda500388c9b096af36334
Спартак <spartak_korolev@mail.ru>
2023-12-09 20:42:34 +0300

    Step 3. Pack using in labs

--- all-the-overhaul-modpack/prototypes/items.lua
+++ all-the-overhaul-modpack/prototypes/items.lua
@@ -36,12 +36,34 @@ data:extend({
     stack_size = 50
   },
   {
-    type = "item",
+    type = "tool",
     name = "fusion-science-pack-1",
-    icon = "__all-the-overhaul-modpack__/graphics/icons/fusion-science-pack-1.png",
-    icon_size = 128,
-    subgroup = "data-energy",
+	durability = 1,
+    durability_description_key = "description.science-pack-remaining-amount-key",
+    durability_description_value = "description.science-pack-remaining-amount-value",
+    icon = "__all-the-overhaul-modpack__/graphics/icons/all-fusion-science-pack-1.png",
+    icon_size = 64,
+	icon_mipmaps = 1,
+    subgroup = "energy-science-pack",
     order = "z",
-    stack_size = 200
-  }
+    stack_size = 200,
+	pictures = {
+      {
+        layers = {
+          {
+            filename = "__all-the-overhaul-modpack__/graphics/icons/all-fusion-science-pack-1.png",
+            scale = 0.25,
+            size = 64
+          },
+          {
+            filename = "__space-exploration-graphics__/graphics/icons/catalogue/mask-1.png",
+            scale = 0.25,
+            size = 64,
+            draw_as_light = true
+          }
+        }
+      }
+    }
+  },
+  
 })
