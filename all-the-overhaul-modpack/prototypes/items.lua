4ad0c098ae8cc42ac39a250fb1ba5a3860fd1005
Спартак <spartak_korolev@mail.ru>
2023-12-10 14:33:30 +0300

    Step 5. Add Fusion science pack 3 and 4

--- all-the-overhaul-modpack/prototypes/items.lua
+++ all-the-overhaul-modpack/prototypes/items.lua
@@ -81,6 +81,24 @@ data:extend({
     stack_size = 50
   },
   {
+    type = "item",
+    name = "fusion-catalogue-3",
+    icon = "__all-the-overhaul-modpack__/graphics/icons/fusion-catalogue-3.png",
+    icon_size = 128,
+    subgroup = "data-energy",
+    order = "z",
+    stack_size = 50
+  },
+  {
+    type = "item",
+    name = "fusion-catalogue-4",
+    icon = "__all-the-overhaul-modpack__/graphics/icons/fusion-catalogue-4.png",
+    icon_size = 128,
+    subgroup = "data-energy",
+    order = "z",
+    stack_size = 50
+  },
+  {
     type = "tool",
     name = "fusion-science-pack-1",
 	durability = 1,
@@ -132,6 +150,66 @@ data:extend({
           },
           {
             filename = "__space-exploration-graphics__/graphics/icons/catalogue/mask-2.png",
+            scale = 0.25,
+            size = 64,
+            draw_as_light = true
+          }
+        }
+      }
+    }
+  },
+  {
+    type = "tool",
+    name = "fusion-science-pack-3",
+	durability = 1,
+    durability_description_key = "description.science-pack-remaining-amount-key",
+    durability_description_value = "description.science-pack-remaining-amount-value",
+    icon = "__all-the-overhaul-modpack__/graphics/icons/all-fusion-science-pack-3.png",
+    icon_size = 64,
+	icon_mipmaps = 1,
+    subgroup = "energy-science-pack",
+    order = "z",
+    stack_size = 200,
+	pictures = {
+      {
+        layers = {
+          {
+            filename = "__all-the-overhaul-modpack__/graphics/icons/all-fusion-science-pack-3.png",
+            scale = 0.25,
+            size = 64
+          },
+          {
+            filename = "__space-exploration-graphics__/graphics/icons/catalogue/mask-3.png",
+            scale = 0.25,
+            size = 64,
+            draw_as_light = true
+          }
+        }
+      }
+    }
+  },
+  {
+    type = "tool",
+    name = "fusion-science-pack-4",
+	durability = 1,
+    durability_description_key = "description.science-pack-remaining-amount-key",
+    durability_description_value = "description.science-pack-remaining-amount-value",
+    icon = "__all-the-overhaul-modpack__/graphics/icons/all-fusion-science-pack-4.png",
+    icon_size = 64,
+	icon_mipmaps = 1,
+    subgroup = "energy-science-pack",
+    order = "z",
+    stack_size = 200,
+	pictures = {
+      {
+        layers = {
+          {
+            filename = "__all-the-overhaul-modpack__/graphics/icons/all-fusion-science-pack-4.png",
+            scale = 0.25,
+            size = 64
+          },
+          {
+            filename = "__space-exploration-graphics__/graphics/icons/catalogue/mask-4.png",
             scale = 0.25,
             size = 64,
             draw_as_light = true
