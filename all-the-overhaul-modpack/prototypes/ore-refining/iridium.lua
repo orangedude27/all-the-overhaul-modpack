c184c84c2a78e16e203eb85c2c656090acde63ef
Orangedude27 <mchie.quinn@gmail.comn>
2024-02-18 14:27:55 -0800

    first attempt to get iridium working

---
+++ all-the-overhaul-modpack/prototypes/ore-refining/iridium.lua
@@ -0,0 +1,25 @@
+require('refining')
+
+data:extend(
+    createRefiningData({
+        name = "iridium",
+        itemNames = {
+            ingot = "se-iridium-ingot",
+            dust = "iridium-powder",
+            molten = "molten-tin",
+            plate = "se-iridium-plate",
+            enriched = "enriched-iridium",
+            pure = "se-iridium-blastcake",
+            pellets = "atom-iridium-pellets"
+        },
+        icons = {
+            ingot = { icon = "__space-exploration-graphics__/graphics/icons/iridium-ingot.png", icon_size = 64, icon_mipmaps = 3 },
+            dust = { icon = "__space-exploration-graphics__/graphics/icons/iridium-powder.png", icon_size = 64 },
+            molten = { icon = "__Cobalt__/graphics/icons/molten-cobalt.png", icon_size = 64, icon_mipmaps = 3 },
+            plate = { icon = "__space-exploration-graphics__/graphics/icons/iridium-plate.png", icon_size = 64, icon_mipmaps = 3 },
+            enriched = { icon = "__space-exploration-graphics__/graphics/icons/iridium-blastcake.png", icon_size = 64, icon_mipmaps = 3 },
+            pure = { icon = "__space-exploration-graphics__/graphics/icons/iridium-ore-crushed.png", icon_size = 64 },
+            pellets = { icon = "__all-the-overhaul-modpack__/graphics/icons/atom-tin-pellets.png", icon_size = 128, icon_mipmaps = 3 }
+        }
+    })
+)
