cf298e6639c248aaf35ae0ace35588623576f39a
Simon Brodtmann <github@accounts.brodtmann.info>
2024-02-21 13:39:17 +0100

    Fixes for brass

--- all-the-overhaul-modpack/prototypes/ore-refining/brass.lua
+++ all-the-overhaul-modpack/prototypes/ore-refining/brass.lua
@@ -3,13 +3,15 @@ local config = {
     itemNames = {
         plate = "brass-plate",
         ingot = "brass-ingot",
-        molten = "molten-brass"
+        molten = "atom-brass-molten"
     },
     icons = {
         plate = { icon = "__BrassTacks__/graphics/galdoc/icons/brass-plate.png", icon_size = 64 },
         ingot = { icon = "__BrassTacks__/graphics/galdoc/icons/brass-ingot.png", icon_size = 64 },
         molten = { icon = "__Cobalt__/graphics/icons/molten-cobalt.png", icon_size = 64, icon_mipmaps = 3 } -- TODO
     },
+    additionalResults = {},
+    additionalIngredient = {}
 }
 
 local _ingotToMoltenRecipe = ingotToMoltenRecipe(config)
@@ -22,14 +24,32 @@ data:extend({
     _ingotToMoltenRecipe,
     moltenToPlateRecipe(config),
     {
+        type = "recipe",
+        name = "atom-" .. config.name .. "-plate-dust",
+        icons = {
+            config.icons.plate,
+            createSmallIcon({ icon = "__OD27_5dim_resources__/graphics/icon/resources/5d-copper-dust.png", icon_size = 32, icon_mipmaps = 3 }),
+        },
+        category = "smelting",
+        energy_required = 48,
+        ingredients = {
+            { name = "5d-copper-dust", amount = 11 },
+            { name = "atom-zinc-dust", amount = 11 }
+        },
+        results = {
+            { name = config.itemNames.plate, amount = 15 }
+        },
+        main_product = config.itemNames.plate
+    },
+    {
         type = "fluid",
-        name = "molten-brass",
-        default_temperature = 3422, -- TODO
-        max_temperature = 3422, -- TODO
+        name = "atom-" .. config.name .. "-molten",
+        default_temperature = 232, -- TODO
+        max_temperature = 232, -- TODO
         base_color = { r = 255, g = 160, b = 60 }, -- TODO
         flow_color = { r = 255, g = 160, b = 60 }, -- TODO
         icons = { config.icons.molten },
-        order = "a[molten]-a",
+        order = "a[molten]-b",
         pressure_to_speed_ratio = 0.4,
         flow_to_energy_ratio = 0.59,
         auto_barrel = false,
