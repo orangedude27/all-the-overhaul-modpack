09c66b0fe99bea7725c037b057520bc20a1c7112
Simon Brodtmann <github@accounts.brodtmann.info>
2024-02-21 13:04:22 +0100

    Fixes for tantalum and biobium

--- all-the-overhaul-modpack/prototypes/ore-refining/tantalite.lua
+++ all-the-overhaul-modpack/prototypes/ore-refining/tantalite.lua
@@ -21,37 +21,92 @@ local tantalumConfig = {
     },
     additionalResults = {
         dustToPure = { { name = "fi-materials-pure-niobium", amount = 1 } },
-    }
+        dustToPlate = { { name = "niobium-plate", amount = 1 } },
+    },
+    additionalIngredient = {}
 }
 
-data:extend(createRefiningData(tantalumConfig))
-
-data:extend({
-    item(tantalumConfig, "dust")
-})
-
---[[
 local niobiumConfig = {
     name = "niobium",
     itemNames = {
+        plate = "niobium-plate",
         ingot = "niobium-ingot",
         molten = "molten-niobium",
-        plate = "niobium-plate",
+        enriched = "atom-niobium-enriched",
+        pure = "atom-niobium-pure",
+        pellets = "atom-niobium-pellets"
     },
     icons = {
-        ingot = { icon = "__Tantalite__/graphics/icons/niobium-ingot.png", icon_size = 64 },
-        molten = { icon = "__Tantalite__/graphics/icons/molten-tantalite.png", icon_size = 64 },
         plate = { icon = "__Tantalite__/graphics/icons/niobium-plate.png", icon_size = 64, icon_mipmaps = 3 },
-        enriched = { icon = "__Tantalite__/graphics/icons/enriched-tantalite.png", icon_size = 64 },
+        ingot = { icon = "__Tantalite__/graphics/icons/niobium-ingot.png", icon_size = 64 },
+        molten = { icon = "__Tantalite__/graphics/icons/molten-tantalite.png", icon_size = 64 }, -- TODO
+        enriched = { icon = "__Tantalite__/graphics/icons/enriched-tantalite.png", icon_size = 64 }, -- TODO
         pure = { icon = "__Tantalite__/graphics/icons/fi-materials-pure-niobium.png", icon_size = 64 },
-        pellets = { icon = "__all-the-overhaul-modpack__/graphics/icons/atom-tantalite-pellets.png", icon_size = 128 }
+        pellets = { icon = "__all-the-overhaul-modpack__/graphics/icons/atom-tantalite-pellets.png", icon_size = 128 } -- TODO
     },
+    additionalResults = {},
+    additionalIngredient = {}
+}
+
+local _dustToPlateRecipe = dustToPlateRecipe(tantalumConfig)
+local _dustToIngotRecipe = dustToIngotRecipe(tantalumConfig)
+local _dustToEnrichedRecipe = dustToEnrichedRecipe(tantalumConfig)
+local _dustToPureRecipe = dustToPureRecipe(tantalumConfig)
+
+_dustToPlateRecipe.results = {
+    { name = tantalumConfig.itemNames.plate, amount_min = 7, amount_max = 8 },
+    { name = niobiumConfig.itemNames.plate, amount_min = 7, amount_max = 8 }
+}
+_dustToIngotRecipe.results = {
+    { name = tantalumConfig.itemNames.ingot, amount = 2 },
+    { name = niobiumConfig.itemNames.ingot, amount = 2 }
+}
+_dustToEnrichedRecipe.results = {
+    { name = tantalumConfig.itemNames.enriched, amount = 1 },
+    { name = niobiumConfig.itemNames.enriched, amount = 1 }
+}
+_dustToPureRecipe.results = {
+    { name = tantalumConfig.itemNames.pure, amount = 1, probability = 0.5 },
+    { name = niobiumConfig.itemNames.pure, amount = 1, probability = 0.5 }
 }
 
 data:extend({
-    dustToIngotRecipe(config),
-    enrichedToIngotRecipe(config),
-    pelletsToIngotRecipe(config),
-    item(config, "pellets")
+    _dustToPlateRecipe,
+    _dustToIngotRecipe,
+    _dustToEnrichedRecipe,
+    _dustToPureRecipe,
+    ingotToMoltenRecipe(tantalumConfig),
+    moltenToPlateRecipe(tantalumConfig),
+    enrichedToIngotRecipe(tantalumConfig),
+    pureToEnrichedRecipe(tantalumConfig),
+    enrichedToPelletsRecipe(tantalumConfig),
+    pelletsToIngotRecipe(tantalumConfig),
+    item(tantalumConfig, "pellets"),
+    item(tantalumConfig, "dust")
 })
-]]--
+
+data:extend({
+    ingotToMoltenRecipe(niobiumConfig),
+    moltenToPlateRecipe(niobiumConfig),
+    enrichedToIngotRecipe(niobiumConfig),
+    pureToEnrichedRecipe(niobiumConfig),
+    enrichedToPelletsRecipe(niobiumConfig),
+    pelletsToIngotRecipe(niobiumConfig),
+    item(niobiumConfig, "pellets"),
+    item(niobiumConfig, "enriched"),
+    item(niobiumConfig, "pure"),
+    {
+        type = "fluid",
+        name = "molten-niobium",
+        default_temperature = 232,
+        max_temperature = 232,
+        base_color = {r=191, g=219, b=233}, -- TODO
+        flow_color = {r=191, g=219, b=233}, -- TODO
+        icons = {{icon = "__Tantalite__/graphics/icons/molten-tantalite.png", icon_size = 64}}, -- TODO
+        order = "a[molten]-a",
+        pressure_to_speed_ratio = 0.4,
+        flow_to_energy_ratio = 0.59,
+        auto_barrel = false,
+        subgroup = "fluid",
+    }
+})
