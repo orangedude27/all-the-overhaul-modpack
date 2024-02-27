45ad8cf18997ca9f24faa096b7de14be1092639b
Simon Brodtmann <github@accounts.brodtmann.info>
2024-02-27 10:37:17 +0100

    Cleanup

--- all-the-overhaul-modpack/prototypes/ore-refining/refining-fixes.lua
+++ all-the-overhaul-modpack/prototypes/ore-refining/refining-fixes.lua
@@ -2,6 +2,3 @@ local Recipe = require('__stdlib__/stdlib/data/recipe')
 local atom_util = require("data-util")
 
 require("fixes.fixes")
-
--- Lead Recipes to Disable
---data.raw.recipe["enriched-lead"].category = "fluid-filtration"
