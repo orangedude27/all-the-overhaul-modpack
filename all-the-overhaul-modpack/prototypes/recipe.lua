40cb19893ed5f81665e01402bbeb0844170692d3
SimonBrodtmann <SimonBrodtmann@users.noreply.github.com>
2024-03-06 00:47:54 +0100

    Add new graphite recipes (#24)
    * Remove OP graphite recipe
    
    * Add new graphite recipes

--- all-the-overhaul-modpack/prototypes/recipe.lua
+++ all-the-overhaul-modpack/prototypes/recipe.lua
@@ -2333,3 +2333,52 @@ Recipe("5d-radar-10"):replace_ingredient("low-density-structure", { "hv-power-re
 Recipe("5d-radar-10"):replace_ingredient("steel-plate", { "se-nanomaterial", 5 })
 Recipe("5d-radar-10"):add_ingredient({ "se-aeroframe-pole", 2 })
 Recipe("5d-radar-10"):add_ingredient({ "se-aeroframe-bulkhead", 5 })
+
+-- Add coke and coal to graphite recipes
+data:extend({
+    {
+        type = "recipe",
+        name = "raw-graphite-coke",
+        icons = {
+            { icon = "__Nylon__/graphics/icons/raw-graphite.png", icon_size = 128 },
+            createSmallIcon({ icon = "__Krastorio2Assets__/icons/items-with-variations/coke/coke.png", icon_size = 64, icon_mipmaps = 4 })
+        },
+        category = "biofarm-mod-smelting",
+        order = "d[graphite]",
+        enabled = false,
+        energy_required = 1.6,
+        ingredients = { { "coke", 1 } },
+        results = { { "raw-graphite", 1 } },
+    },
+    {
+        type = "recipe",
+        name = "raw-graphite-charcoal",
+        icons = {
+            { icon = "__Nylon__/graphics/icons/raw-graphite.png", icon_size = 128 },
+            createSmallIcon({ icon = "__Bio_Industries__/graphics/icons/charcoal.png", icon_size = 64, icon_mipmaps = 4 })
+        },
+        category = "biofarm-mod-smelting",
+        order = "d[graphite]",
+        enabled = false,
+        energy_required = 1.6,
+        ingredients = { { "wood-charcoal", 1 } },
+        results = { { "raw-graphite", 1 } },
+    },
+    {
+        type = "recipe",
+        name = "raw-graphite-coal",
+        icons = {
+            { icon = "__Nylon__/graphics/icons/raw-graphite.png", icon_size = 128 },
+            createSmallIcon(atom.refining.config.coal.icons.dust)
+        },
+        category = "biofarm-mod-smelting",
+        order = "d[graphite]",
+        enabled = false,
+        energy_required = 1.6,
+        ingredients = { { atom.refining.config.coal.itemNames.dust, 2 } },
+        results = { { "raw-graphite", 1 } },
+    }
+})
+table.insert(data.raw.technology["bi-tech-coal-processing-1"].effects, { type = "unlock-recipe", recipe = "raw-graphite-coke" })
+table.insert(data.raw.technology["bi-tech-coal-processing-1"].effects, { type = "unlock-recipe", recipe = "raw-graphite-charcoal" })
+table.insert(data.raw.technology["bi-tech-coal-processing-1"].effects, { type = "unlock-recipe", recipe = "raw-graphite-coal" })
