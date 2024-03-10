6e79c8e19a1f17972f952e34e8cf200a81edd415
Simon Brodtmann <github@accounts.brodtmann.info>
2024-03-10 10:17:28 +0100

    Remove pure neodymium and fluid

--- all-the-overhaul-modpack/integrations/248k.lua
+++ all-the-overhaul-modpack/integrations/248k.lua
@@ -3,3 +3,7 @@ data.raw.technology["fu_gold_ingot_tech"] = nil
 data.raw.technology["fu_lead_ingot_tech"] = nil
 data.raw.technology["fu_titan_ingot_tech"] = nil
 data.raw.technology["fu_neodym_ingot_tech"] = nil
+
+-- Remove pure neodymium and fluid
+atom.util.item.removeByName("fi_materials_pure_neodym")
+atom.util.item.removeByName("fi_arc_neodym")
