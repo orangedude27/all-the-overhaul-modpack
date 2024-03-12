2b16543c86a7af4c7a65b02856f07abecc72259b
Simon Brodtmann <github@accounts.brodtmann.info>
2024-03-12 21:53:06 +0100

    Cleanup

--- all-the-overhaul-modpack/content/material-processing/fixes.lua
+++ all-the-overhaul-modpack/content/material-processing/fixes.lua
@@ -15,7 +15,3 @@ require("fixes.trikop")
 -- Remove vanilla plate recipes
 atom.util.recipe.removeByName("iron-plate")
 atom.util.recipe.removeByName("copper-plate")
-
--- for some reason normal pipes sometimes don't have the correct collision_mask, so just force them to match the steel pipes
-data.raw["pipe"]["pipe"].collision_mask = data.raw["pipe"]["kr-steel-pipe"].collision_mask
-data.raw["pipe-to-ground"]["pipe-to-ground"].collision_mask = data.raw["pipe-to-ground"]["kr-steel-pipe-to-ground"].collision_mask
--- all-the-overhaul-modpack/data-final-fixes.lua
+++ all-the-overhaul-modpack/data-final-fixes.lua
@@ -4,3 +4,7 @@ require("_data-final-fixes")
 
 require("content.fixes")
 require("integrations.fixes")
+
+-- for some reason normal pipes sometimes don't have the correct collision_mask, so just force them to match the steel pipes
+data.raw["pipe"]["pipe"].collision_mask = data.raw["pipe"]["kr-steel-pipe"].collision_mask
+data.raw["pipe-to-ground"]["pipe-to-ground"].collision_mask = data.raw["pipe-to-ground"]["kr-steel-pipe-to-ground"].collision_mask
