b2ff17b2e3d4e9ce4e97ea8b0546c652e59bdfb9
Simon Brodtmann <github@accounts.brodtmann.info>
2024-04-04 22:28:38 +0200

    Make fuel categories of bio boiler match vanilla boiler

--- all-the-overhaul-modpack/integrations/bio-industries.lua
+++ all-the-overhaul-modpack/integrations/bio-industries.lua
@@ -4,6 +4,9 @@ atom.util.recipe.removeByName("bi-rail-wood-to-concrete")
 atom.util.recipe.removeByName("bi-plastic-1")
 atom.util.recipe.removeByName("bi-plastic-2")
 
+-- Make fuel categories of bio boiler match vanilla boiler
+data.raw.boiler["bi-bio-boiler"].energy_source.fuel_categories = data.raw.boiler["boiler"].energy_source.fuel_categories
+
 -- K2
 table.insert(data.raw["assembling-machine"]["kr-greenhouse"].crafting_categories, "biofarm-mod-farm")
 table.insert(data.raw["assembling-machine"]["kr-greenhouse"].crafting_categories, "biofarm-mod-greenhouse")
