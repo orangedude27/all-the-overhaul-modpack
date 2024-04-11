8ec7dfcbd09b2140b4e7e757a7653a4bb2424109
Simon Brodtmann <github@accounts.brodtmann.info>
2024-04-11 21:27:00 +0200

    Fix handcrafting wooden pipe to ground
    https://discord.com/channels/1174743568590590053/1219277139975737354

--- all-the-overhaul-modpack/integrations/bio-industries.lua
+++ all-the-overhaul-modpack/integrations/bio-industries.lua
@@ -7,6 +7,10 @@ atom.util.recipe.removeByName("bi-plastic-2")
 -- Make fuel categories of bio boiler match vanilla boiler
 data.raw.boiler["bi-bio-boiler"].energy_source.fuel_categories = data.raw.boiler["boiler"].energy_source.fuel_categories
 
+-- Fix handcrafting wooden pipe to ground
+data.raw.recipe["bi-wood-pipe"].normal.allow_as_intermediate = true
+data.raw.recipe["bi-wood-pipe"].expensive.allow_as_intermediate = true
+
 -- K2
 table.insert(data.raw["assembling-machine"]["kr-greenhouse"].crafting_categories, "biofarm-mod-farm")
 table.insert(data.raw["assembling-machine"]["kr-greenhouse"].crafting_categories, "biofarm-mod-greenhouse")
