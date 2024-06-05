9c9a5c2f82b737cacd8cb02649521b4eb0728b7a
Orangedude27 <mchie.quinn@gmail.comn>
2024-06-04 23:40:10 -0700

    move the fluid points around

--- all-the-overhaul-modpack/integrations/lazing-around.lua
+++ all-the-overhaul-modpack/integrations/lazing-around.lua
@@ -3,14 +3,14 @@
 -- Fixes Advanced circuit (the recipe uses two fluids but the building only supports one)
 -- atom.util.Recipe("advanced-circuit-in-laser-mill").removeIngredient("helium")
 
-data.raw["assembling-machine"]["laser-mill"].fluid_boxes[1].pipe_connections = {{ type = "input", position = { 1, -2 }}}
+-- data.raw["assembling-machine"]["laser-mill"].fluid_boxes[1].pipe_connections = {{ type = "input", position = { 1, -2 }}}
 newInput = {
   production_type = "input",
   pipe_picture = data.raw["assembling-machine"]["laser-mill"].fluid_boxes[1].pipe_picture,
   pipe_covers = data.raw["assembling-machine"]["laser-mill"].fluid_boxes[1].pipe_covers,
   base_area = 10,
   base_level = -1,
-  pipe_connections = {{ type = "input", position = { -1, -2 }}},
+  pipe_connections = {{ type = "input", position = { -2, -2 }}},
   secondary_draw_orders = { north = -1, east = -1, west = -1 }
 }
 table.insert(data.raw["assembling-machine"]["laser-mill"].fluid_boxes, newInput)
