1443824e5e03d6db37101802e6dedcb47c92751d
Simon Brodtmann <github@accounts.brodtmann.info>
2024-06-13 21:01:18 +0200

    Laser mill: Add additional pipe connection for second fluid input

--- all-the-overhaul-modpack/integrations/lazing-around.lua
+++ all-the-overhaul-modpack/integrations/lazing-around.lua
@@ -1,11 +1,14 @@
 -- Add second fluid box to laser-mill to enable recipes with two fluids (one being helium)
-newInput = {
+local newInput = {
   production_type = "input",
   pipe_picture = data.raw["assembling-machine"]["laser-mill"].fluid_boxes[1].pipe_picture,
   pipe_covers = data.raw["assembling-machine"]["laser-mill"].fluid_boxes[1].pipe_covers,
   base_area = 10,
   base_level = -1,
-  pipe_connections = {{ type = "input", position = { -2, 0 }}},
+  pipe_connections = {
+    { type = "input", position = { -2, 0 }},
+    { type = "input", position = { 2, 0 }}
+  },
   secondary_draw_orders = { north = -1, east = -1, west = -1 }
 }
 table.insert(data.raw["assembling-machine"]["laser-mill"].fluid_boxes, newInput)
