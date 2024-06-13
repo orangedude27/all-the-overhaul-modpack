fed37a486067e0bd0d5cebb21056fa98516fea69
Simon Brodtmann <github@accounts.brodtmann.info>
2024-06-13 14:58:19 +0200

    Use purple tech cards more
    https://discord.com/channels/1174743568590590053/1242953953822642216

--- all-the-overhaul-modpack/integrations/final.lua
+++ all-the-overhaul-modpack/integrations/final.lua
@@ -1,1 +1,32 @@
 require("248k.final")
+
+-- Use purple tech cards more
+local function isIngredient(_table, name)
+    if (_table == nil) then
+        return false
+    end
+    return _table[1] == name or _table.name == name
+end
+for _, technology in pairs(data.raw.technology) do
+    local hasRocket = false
+    local hasChemical = false
+    local hasSpace = false
+    local hasProductivity = false
+    for _, ingredient in pairs(technology.unit.ingredients) do
+        if isIngredient(ingredient, "se-rocket-science-pack") then
+            hasRocket = true
+        end
+        if isIngredient(ingredient, "chemical-science-pack") then
+            hasChemical = true
+        end
+        if isIngredient(ingredient, "space-science-pack") then
+            hasSpace = true
+        end
+        if isIngredient(ingredient, "productivity-science-pack") then
+            hasProductivity = true
+        end
+    end
+    if hasChemical and hasRocket and hasSpace and not hasProductivity then
+        atom.util.Technology(technology).addIngredient("productivity-science-pack")
+    end
+end
