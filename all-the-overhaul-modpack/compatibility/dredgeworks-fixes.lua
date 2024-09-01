801b9154166f43d129b4d79c9279acc97e4dc887
Simon Brodtmann <github@accounts.brodtmann.info>
2024-09-01 21:46:03 +0200

    Fix crash for when the mod Dredgeworks is not active

--- all-the-overhaul-modpack/compatibility/dredgeworks-fixes.lua
+++ all-the-overhaul-modpack/compatibility/dredgeworks-fixes.lua
@@ -1,4 +1,6 @@
--- Re-add ingredients removed by K2/SE
-local mk3Tech = atom.util.Technology("floating-drill-mk3-technology")
-mk3Tech.addIngredient("automation-science-pack")
-mk3Tech.addIngredient("logistic-science-pack")
+if (mods["dredgeworks"]) then
+    -- Re-add ingredients removed by K2/SE
+    local mk3Tech = atom.util.Technology("floating-drill-mk3-technology")
+    mk3Tech.addIngredient("automation-science-pack")
+    mk3Tech.addIngredient("logistic-science-pack")
+end
