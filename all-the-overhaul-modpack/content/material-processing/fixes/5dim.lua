0e0cfa05e24ff9c09af4416e614ee96706471e6f
Simon Brodtmann <github@accounts.brodtmann.info>
2024-09-26 23:04:46 +0200

    Fix removal of recipes generated by other mods
    https://discord.com/channels/1174743568590590053/1288898951373787167

--- all-the-overhaul-modpack/content/material-processing/fixes/5dim.lua
+++ all-the-overhaul-modpack/content/material-processing/fixes/5dim.lua
@@ -1,5 +1,5 @@
 -- Remove ore to dust recipes
-atom.util.recipe.removeByNamePattern("5d%-%a+%-dust")
+atom.util.recipe.removeByNamePattern("^5d%-%a+%-dust")
 
 -- Remove smelting of dust to plate recipes
-atom.util.recipe.removeByNamePattern("5d%-%a+%-plate")
+atom.util.recipe.removeByNamePattern("^5d%-%a+%-plate")
