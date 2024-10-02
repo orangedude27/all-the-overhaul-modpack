ecea3988bf0e58d8c750229aebfe423aaaedf191
Simon Brodtmann <github@accounts.brodtmann.info>
2024-10-02 08:01:04 +0200

    Fix crash when "Enable waste treatment" from BrimStuff is enabled
    https://discord.com/channels/1174743568590590053/1290871879065800795

--- all-the-overhaul-modpack/integrations/planetfall.lua
+++ all-the-overhaul-modpack/integrations/planetfall.lua
@@ -1,2 +1,4 @@
--- Change subgroup for nodule scrapping
-data.raw.recipe["nodule-scrapping"].subgroup = "recycle"
+if settings.startup["brimstuff-waste-treatment"].value then
+    -- Change subgroup for nodule scrapping
+    data.raw.recipe["nodule-scrapping"].subgroup = "recycle"
+end
