337eda01c26fab54e2e6e674f49868b1ce07f6c4
Simon Brodtmann <github@accounts.brodtmann.info>
2025-11-02 17:24:37 +0100

    Chemical plant mk1 requires tungsten so we move it further down

--- all-the-overhaul-modpack/data-updates.lua
+++ all-the-overhaul-modpack/data-updates.lua
@@ -1,2 +1,6 @@
 require("integrations/updates")
 require("compatibility/updates")
+
+-- Chemical plant mk1 requires tungsten so we move it further down
+atom.util.Technology("kr-fluids-chemistry").removeRecipe("chemical-plant")
+atom.util.Technology("oil-processing").addRecipe("chemical-plant")
