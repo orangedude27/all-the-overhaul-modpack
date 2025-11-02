55d2145a35b5aad89a00b9eced085f8cff5dafc7
Simon Brodtmann <github@accounts.brodtmann.info>
2025-11-02 16:27:57 +0100

    Add steel processing to ore purification prerequisites and remove electronics technology stage

--- all-the-overhaul-modpack/integrations/248k/data.lua
+++ all-the-overhaul-modpack/integrations/248k/data.lua
@@ -1,3 +1,10 @@
 -- Balance solar
 -- Set both in data and final-fixes because of 248k/scripts/krastorio2/overhaul.lua and solar productivity mod using data-updates phase
 data.raw["solar-panel"]["el_solar_entity"].production = "200kW"
+
+-- Technologies
+local orePurification = atom.util.Technology("el_purifier_tech")
+orePurification.addPrerequisite("steel-processing")
+orePurification.replacePrerequisite("el_stage_tech", "electronics")
+orePurification.removePrerequisite("logistic-science-pack")
+data.raw.technology["el_stage_tech"] = nil
