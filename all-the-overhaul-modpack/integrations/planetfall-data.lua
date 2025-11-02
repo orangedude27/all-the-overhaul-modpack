ec494e374aeab4a246421f57211252c45c646a47
Simon Brodtmann <github@accounts.brodtmann.info>
2025-11-02 09:19:04 +0100

    Fix name for potassium-nitrate-sand

--- all-the-overhaul-modpack/integrations/planetfall-data.lua
+++ all-the-overhaul-modpack/integrations/planetfall-data.lua
@@ -1,6 +1,7 @@
 -- Alternative recipe for potassium nitrate with sand instead of stone
 local potassiumNitrate = atom.util.Recipe(table.deepcopy(data.raw.recipe["potassium-nitrate"]))
 potassiumNitrate.prototype.name = "potassium-nitrate-sand"
+potassiumNitrate.prototype.localised_name = { "item-name.potassium-nitrate" }
 potassiumNitrate.prototype.icons = {
     { icon = "__BrimStuff-Updated__/graphics/icons/potassium-nitrate.png", icon_size = 64 },
     atom.util.icon.createSmallIcon(atom.processing.materials.stone.icons.sand)
