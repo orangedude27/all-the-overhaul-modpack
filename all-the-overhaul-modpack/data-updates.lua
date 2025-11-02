0f3bcae2127509505835b178694466be4af1a30e
Simon Brodtmann <github@accounts.brodtmann.info>
2025-11-02 18:20:03 +0100

    Change tech icon of oil processing back to vanilla

--- all-the-overhaul-modpack/data-updates.lua
+++ all-the-overhaul-modpack/data-updates.lua
@@ -4,3 +4,8 @@ require("compatibility/updates")
 -- Chemical plant mk1 requires tungsten so we move it further down
 atom.util.Technology("kr-fluids-chemistry").removeRecipe("chemical-plant")
 atom.util.Technology("oil-processing").addRecipe("chemical-plant")
+
+-- Change tech icon of oil processing back to vanilla
+data.raw.technology["oil-processing"].icon = "__base__/graphics/technology/oil-processing.png"
+data.raw.technology["oil-processing"].icon_size = 256
+data.raw.technology["oil-processing"].icons = nil
