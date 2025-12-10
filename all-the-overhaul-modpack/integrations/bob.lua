65c7b27a095ccf6904c21012996a4039da3e94fd
Simon Brodtmann <github@accounts.brodtmann.info>
2025-12-10 20:55:47 +0100

    Move resin from heavy oil from oil processing to advanced oil processing

--- all-the-overhaul-modpack/integrations/bob.lua
+++ all-the-overhaul-modpack/integrations/bob.lua
@@ -5,3 +5,7 @@ bec.replaceIngredient("graphite", 5)
 
 atom.util.Recipe("electronic-circuit").replaceIngredient("basic-electronic-components", 2)
 atom.util.Recipe("electronic-circuit-in-laser-mill").replaceIngredient("basic-electronic-components", 2)
+
+-- Move resin from heavy oil from oil processing to advanced oil processing
+atom.util.Technology("oil-processing").removeRecipe("bob-resin-oil")
+atom.util.Technology("advanced-oil-processing").addRecipe("bob-resin-oil")
