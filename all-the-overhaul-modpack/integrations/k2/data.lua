a36d0f1c1ab0e0f38b9d3462601aa71800bc2b4a
Simon Brodtmann <github@accounts.brodtmann.info>
2025-12-10 21:03:51 +0100

    Add Advanced multi cylinder engine as prerequisite to Atmospheric condensation

--- all-the-overhaul-modpack/integrations/k2/data.lua
+++ all-the-overhaul-modpack/integrations/k2/data.lua
@@ -1,3 +1,5 @@
 -- Balance solar
 -- Set both in data and final-fixes because of 248k/scripts/krastorio2/overhaul.lua and solar productivity mod using data-updates phase
 data.raw["solar-panel"]["kr-advanced-solar-panel"].production = "300kW"
+
+atom.util.Technology("kr-atmosphere-condensation").addPrerequisite("advanced-multi-cylinder-engine")
