3ba5fa8944db901d9267fd178e82df9f8c338cd1
Morganite-MLI <agemoagemo@yahoo.com>
2026-01-05 23:10:19 -0600

    Allow 5dim mashers in space

--- all-the-overhaul-modpack/integrations/5dim/updates.lua
+++ all-the-overhaul-modpack/integrations/5dim/updates.lua
@@ -1,1 +1,12 @@
 require("lab")
+
+data.raw["furnace"]["5d-masher-01"].se_allow_in_space = true
+data.raw["furnace"]["5d-masher-02"].se_allow_in_space = true
+data.raw["furnace"]["5d-masher-03"].se_allow_in_space = true
+data.raw["furnace"]["5d-masher-04"].se_allow_in_space = true
+data.raw["furnace"]["5d-masher-05"].se_allow_in_space = true
+data.raw["furnace"]["5d-masher-06"].se_allow_in_space = true
+data.raw["furnace"]["5d-masher-07"].se_allow_in_space = true
+data.raw["furnace"]["5d-masher-08"].se_allow_in_space = true
+data.raw["furnace"]["5d-masher-09"].se_allow_in_space = true
+data.raw["furnace"]["5d-masher-10"].se_allow_in_space = true
